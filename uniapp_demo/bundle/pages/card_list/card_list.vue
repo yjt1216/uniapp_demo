8e99d348fbf6cf811910d61071d28effa9d4e081
jtyin <jtyin@depingroup.com>
2023-05-17 15:06:43 +0800

    feat(package.json): 添加package.json

--- uniapp_demo/bundle/pages/card_list/card_list.vue
+++ uniapp_demo/bundle/pages/card_list/card_list.vue
@@ -1,8 +1,25 @@
 <template>
+	<view class="card-list">
+		<nav-bar title="卡片列表" backgroundColor="linear-gradient(to right, #39C9BC,#6DE8CC)"></nav-bar>
+	</view>
 </template>
 
 <script>
+	export default {
+		data(){
+			
+		},
+		onLoad() {
+			console.log('卡片列表 加载')
+		},
+		methods:{
+			
+		}
+	}
 </script>
 
-<style>
+<style lang="scss">
+	.card-list {
+		
+	}
 </style>
